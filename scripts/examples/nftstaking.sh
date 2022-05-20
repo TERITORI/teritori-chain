643ef2d6da6fafb4c44f4dfd267b7426865e51c5
gopherjk121 <strdev123strdev@gmail.com>
2022-05-20 12:38:53 +1000

    test and resolve nftstaking amount

--- scripts/examples/nftstaking.sh
+++ scripts/examples/nftstaking.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-nxtpopd tx nftstaking register-nft-staking --from validator --nft-identifier "identifier" --nft-metadata "metadata" --reward-address "pop1hw...tfh" --reward-weight 1000 --chain-id=testing --home=$HOME/.nxtpopd --keyring-backend=test --broadcast-mode=block --yes
+nxtpopd tx nftstaking register-nft-staking --from validator --nft-identifier "identifier3" --nft-metadata "metadata" --reward-address "pop1snktzg6rrncqtct3acx2vz60aak2a6fke3ny3c" --reward-weight 1000 --chain-id=testing --home=$HOME/.nxtpopd --keyring-backend=test --broadcast-mode=block --yes
 
-nxtpopd query bank balances pop1hw...tfh
+nxtpopd query bank balances pop1uef5c6tx7vhjyhfumhzdhvwkepshcmljyv4wh4
