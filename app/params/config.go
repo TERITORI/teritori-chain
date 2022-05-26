4d5991d6756367c19de263c13d73554fdae69a21
gopherjk121 <strdev123strdev@gmail.com>
2022-05-26 22:21:53 +1000

    update address prefix

--- app/params/config.go
+++ app/params/config.go
@@ -11,7 +11,7 @@ const (
 	DefaultBondDenom = BaseCoinUnit
 
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = "teri"
+	Bech32PrefixAccAddr = "tori"
 )
 
 var (
