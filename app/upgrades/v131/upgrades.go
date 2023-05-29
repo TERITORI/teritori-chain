01f60ec7fb9cb9d77dbe7fee1a9d69ff4fb0d2b9
gopherjk121 <strdev123strdev@gmail.com>
2023-05-29 21:41:03 +0800

    resolve build error on v131 upgrade handler

--- app/upgrades/v131/upgrades.go
+++ app/upgrades/v131/upgrades.go
@@ -1,6 +1,7 @@
 package v131
 
 import (
+	"github.com/TERITORI/teritori-chain/app/keepers"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -9,6 +10,7 @@ import (
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	keepers keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("start to run module migrations...")
