20a31ce3e7fc90432054a9783e5868c0c217fb42
Norman Meier <norman@samourai.coop>
2024-01-18 13:22:19 +0100

    feat: basic integration test
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -3,6 +3,17 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
-make install
+commit=267a3f9604d48c6f1ea5c31d13a5d24c6ed35210
 
-parallel --lb -j2 --halt now,done=1 ::: './start.sh >> node_logs.txt 2>&1' './integration-test/test.sh'
+if [[ -z "${TERITORI_DAPP_REPO:-}" ]]; then
+    rm -fr teritori-dapp
+    git clone https://github.com/TERITORI/teritori-dapp.git
+    cd teritori-dapp
+    git checkout $commit
+else
+    cd $TERITORI_DAPP_REPO
+fi
+
+yarn
+
+npx tsx packages/scripts/integration-testing/upgradeTest142toDir ..
