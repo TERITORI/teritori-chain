04850224c72a08c7ba1cdf468a02378016de057d
go7066 <72454960+go7066@users.noreply.github.com>
2024-01-22 21:23:19 +0800

    fix: add ConsensusParams upgrade and query (#84)
    * add consensus params fix
    
    * chore: update test
    
    Signed-off-by: Norman Meier <norman@samourai.coop>
    
    * chore: update test in ci
    
    Signed-off-by: Norman Meier <norman@samourai.coop>
    
    * update comment for consensus params query cli
    
    ---------
    
    Signed-off-by: Norman Meier <norman@samourai.coop>
    Co-authored-by: Norman Meier <norman@samourai.coop>

--- app/upgrades/v200/upgrades.go
+++ app/upgrades/v200/upgrades.go
@@ -6,6 +6,8 @@ import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/TERITORI/teritori-chain/app/keepers"
 	minttypes "github.com/TERITORI/teritori-chain/x/mint/types"
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	tmtypes "github.com/cometbft/cometbft/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -71,6 +73,14 @@ func CreateUpgradeHandler(
 		subspace.Set(ctx, minttypes.KeyBlocksPerYear, v)
 		v = reflect.Indirect(reflect.ValueOf(params.TotalBurntAmount)).Interface()
 		subspace.Set(ctx, minttypes.KeyTotalBurntAmount, v)
+
+		cp := tmtypes.DefaultConsensusParams().ToProto()
+		keepers.ConsensusParamsKeeper.Set(ctx, &tmproto.ConsensusParams{
+			Block:     cp.Block,
+			Validator: cp.Validator,
+			Evidence:  cp.Evidence,
+			Version:   cp.Version,
+		})
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
